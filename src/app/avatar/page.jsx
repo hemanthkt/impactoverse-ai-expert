46f0d5f50972e04a97edfbf26f66caabf2848f15
hemanthkt <hmkt1503@gmail.com>
2025-07-13 01:47:21 +0530

    firt

---
+++ src/app/avatar/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+import ExpertSelection from "@/components/ExpertSelection";
+import React from "react";
+
+export default function AvatarPage() {
+  return (
+    <div>
+      <ExpertSelection />
+    </div>
+  );
+}
---
+++ src/app/login/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+import Login from "@/components/Login";
+import React from "react";
+
+export default function LoginPage() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
